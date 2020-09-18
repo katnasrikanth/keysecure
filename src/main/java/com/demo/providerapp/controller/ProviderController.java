d120aa9bf6aee5e835dc85c4dc598ab4f282d453
katnasrikanth <srish789@gmail.com>
2020-09-18 17:57:12 +0530

    new changes

--- src/main/java/com/demo/providerapp/controller/ProviderController.java
+++ src/main/java/com/demo/providerapp/controller/ProviderController.java
@@ -3,6 +3,7 @@ package com.demo.providerapp.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
